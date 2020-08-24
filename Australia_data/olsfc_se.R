c9490d7e421fa9083f197febc3ffdc6044be23d5
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2020-08-24 15:24:03 +0800

    added PI for first part of simulationed

--- Australia_data/olsfc_se.R
+++ Australia_data/olsfc_se.R
@@ -2,15 +2,13 @@
 ## OLS forecasts
 ## Assumes x is a ts object with frequency set correctly
 ## Produces forecasts from a linear model with trend, seasonal dummy variables
-## and (optionally) some autoregressive terms (lagno = vector of the lag number which we want to include in the model)
-## this function returns fc.se (the forecast standard errors for computing prediction intervals)
+## and (optionally) some autoregressive terms (lagno = vector of the lag number which we want to include in the model )
 
-olsfc.se <- function(x, h, maxlag = 0, nolag = NULL ) {
+olsfc <- function(x, h, maxlag = 0, nolag = NULL ) {
   # Set up data frame for modelling
   n <- length(x)
   modeldata <- data.frame(
     x = as.numeric(x),
-    #trend1 = (seq_along(x))^2,
     trend = seq_along(x),
     season = factor(cycle(x))
   )
@@ -25,7 +23,6 @@ olsfc.se <- function(x, h, maxlag = 0, nolag = NULL ) {
   # Set up formula for linear model
   if (length(nolag) == 0)
     nolag <- seq(maxlag) 
-  #form <- "x ~ trend1 + trend2 + season"
   form <- "x ~ trend + season"
   for (i in nolag)
     form <- paste0(form, " + ", "lag", i)
@@ -39,16 +36,22 @@ olsfc.se <- function(x, h, maxlag = 0, nolag = NULL ) {
            frequency = frequency(x),
            start = tsp(x)[2] + 1 / frequency(x)
   )
-  fc.se <- ts(numeric(h),
+  lwr <- ts(numeric(h),
+            frequency = frequency(x),
+            start = tsp(x)[2] + 1 / frequency(x)
+  )
+  upr <- ts(numeric(h),
+            frequency = frequency(x),
+            start = tsp(x)[2] + 1 / frequency(x)
+  )
+  se <- ts(numeric(h),
            frequency = frequency(x),
            start = tsp(x)[2] + 1 / frequency(x)
   )
-  
+  residual.scale <- c() 
   # Set up new data for forecasting
-  trend <- (length(x) + seq(h))^2
-  #trend2 <- length(x) + seq(h)
+  trend <- length(x) + seq(h)
   season <- factor(cycle(fc))
-  #newdata <- data.frame(trend1 = trend1[1], trend2 = trend2[1], season = season[1])
   newdata <- data.frame(trend = trend[1], season = season[1])
   for (i in seq_along(lagnames))
     newdata[[lagnames[i]]] <- tail(x,i)[1]
@@ -56,19 +59,22 @@ olsfc.se <- function(x, h, maxlag = 0, nolag = NULL ) {
   # Compute forecasts
   for (i in seq_along(fc))
   {
-    model.fit <- predict(fit, newdata = newdata, se.fit = TRUE)
-    fc[i] <- model.fit$fit
-    fc.se[i] <-  model.fit$se.fit
+    pred <-  predict(fit, newdata = newdata, se.fit = TRUE, 
+                     residual.scale = TRUE, interval = "prediction", level = 0.95)
+    fc[i] <- pred$fit[,1]
+    lwr[i] <- pred$fit[,2]
+    upr[i] <- pred$fit[,3]
+    se[i] <- pred$se.fit
+    residual.scale[i] <- pred$residual.scale
     # Update newdata
     if(maxlag > 0)
     {
       newdata[lagnames[seq(maxlag)]] <- c(fc[i], newdata[lagnames[1:(maxlag-1)]]) 
-      #for (j in seq_along(lagnames[-1]))
-      #newdata[[lagnames[j+1]]] <- newdata[[lagnames[j]]]
-      #newdata[["lag1"]] <- fc[i]
     }
     newdata[['trend']] <- trend[i+1]
     newdata[['season']] <- season[i+1]
   }
-  return(list(fc, fc.se))
+  return(list(fc, lwr, upr, se, residual.scale))
 }
+
+
